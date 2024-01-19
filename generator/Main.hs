40d04780577288f0caaaab1463d9555403bab9eb
Aura Kelloniemi <kaura.dev@sange.fi>
2024-01-19 16:35:51 +0200

    Use `lovegen` as a library
    LoveGen has become a general-purpose website generation library. We know have
    a specialized generator for this website, which utilizes LoveGen.

---
+++ generator/Main.hs
@@ -0,0 +1,90 @@
+{- |
+Module      : Main
+Description : Lovers' Guild's website generator
+Copyright   : Copyright (C) 2022-2024 The Lovers' Guild
+License     : GNU Affero General Public License version 3
+Maintainer  : dev@loversguild.org
+Stability   : experimental
+Portability : GHC
+-}
+module Main (main) where
+
+import Control.Monad (ap)
+import Data.Time
+import System.Directory.OsPath
+import System.OsPath
+
+import LoveGen
+
+-------------------
+-- Configuration --
+-------------------
+
+finnishSite :: SiteConfig
+finnishSite =
+    defaultSiteConfig
+        { siteUrl = "https://rakastajienkilta.fi/"
+        , pagesDir = [osp|pages/fi|]
+        , outputDir = [osp|output/fi|]
+        , timeLocale = finnishTimeLocale
+        , dateFormat = "%Ana %d. %Bta %Y"
+        }
+
+-- | Finnish time locale
+finnishTimeLocale :: TimeLocale
+finnishTimeLocale =
+    TimeLocale
+        { wDays =
+            ap (,) (take 2)
+                <$> [ "sunnuntai"
+                    , "maanantai"
+                    , "tiistai"
+                    , "keskiviikko"
+                    , "torstai"
+                    , "perjantai"
+                    , "lauantai"
+                    ]
+        , months =
+            ((,) =<< (<> "kuu"))
+                <$> [ "tammi"
+                    , "helmi"
+                    , "maalis"
+                    , "huhti"
+                    , "touko"
+                    , "kesä"
+                    , "heinä"
+                    , "elo"
+                    , "syys"
+                    , "loka"
+                    , "marras"
+                    , "joulu"
+                    ]
+        , amPm = ("ennen puoltapäivää", "puolen päivän jälkeen")
+        , dateTimeFmt = "%d.%m.%Y %H:%M:%S"
+        , dateFmt = "%d.%m.%Y"
+        , timeFmt = "%H:%M:%S"
+        , time12Fmt = "%H:%M:%S"
+        , knownTimeZones = []
+        }
+
+englishSite :: SiteConfig
+englishSite =
+    defaultSiteConfig
+        { siteUrl = "https://loversguild.org/"
+        , pagesDir = [osp|pages/en|]
+        , outputDir = [osp|output/en|]
+        , dateFormat = "on %a, %d %b %Y"
+        }
+
+--------------------------
+-- End of Configuration --
+--------------------------
+
+-- | Main function of the generator
+main :: IO ()
+main = do
+    cliOpts <- parseCmdLine
+    rootDir <- encodeFS cliOpts.directory
+    setCurrentDirectory rootDir
+    buildSite finnishSite
+    buildSite englishSite
