fef76d1d614f41b51392cd5f04a93e8e09e6e7ea
Aura Kelloniemi <kaura.dev@sange.fi>
2024-03-30 18:53:29 +0200

    Adapt to LoveGen's new locale configuration
    There is also some reformatting of the code.

--- generator/Main.hs
+++ generator/Main.hs
@@ -1,12 +1,13 @@
-{- |
-Module      : Main
-Description : Lovers' Guild's website generator
-Copyright   : Copyright (C) 2022-2024 The Lovers' Guild
-License     : GNU Affero General Public License version 3
-Maintainer  : dev@loversguild.org
-Stability   : experimental
-Portability : GHC
--}
+{-# LANGUAGE OverloadedLists #-}
+
+-- |
+-- Module      : Main
+-- Description : Lovers' Guild's website generator
+-- Copyright   : Copyright (C) 2022-2024 The Lovers' Guild
+-- License     : GNU Affero General Public License version 3
+-- Maintainer  : dev@loversguild.org
+-- Stability   : experimental
+-- Portability : GHC
 module Main (main) where
 
 import Control.Monad (ap)
@@ -23,11 +24,17 @@ import LoveGen
 finnishSite :: SiteConfig
 finnishSite =
     defaultSiteConfig
-        { siteUrl = "https://rakastajienkilta.fi/"
-        , pagesDir = [osp|pages/fi|]
-        , outputDir = [osp|output/fi|]
-        , timeLocale = finnishTimeLocale
-        , dateFormat = "%Ana %d. %Bta %Y"
+        { siteUrl = "https://rakastajienkilta.fi/",
+          pagesDir = [osp|pages/fi|],
+          outputDir = [osp|output/fi|],
+          locales =
+            [   ( "fi",
+                  LocaleConfig
+                    { dateFormat = "%Ana %d. %Bta %Y",
+                      timeLocale = finnishTimeLocale
+                    }
+                )
+            ]
         }
 
 -- | Finnish time locale
@@ -36,44 +43,51 @@ finnishTimeLocale =
     TimeLocale
         { wDays =
             ap (,) (take 2)
-                <$> [ "sunnuntai"
-                    , "maanantai"
-                    , "tiistai"
-                    , "keskiviikko"
-                    , "torstai"
-                    , "perjantai"
-                    , "lauantai"
-                    ]
-        , months =
+                <$> [ "sunnuntai",
+                      "maanantai",
+                      "tiistai",
+                      "keskiviikko",
+                      "torstai",
+                      "perjantai",
+                      "lauantai"
+                    ],
+          months =
             ((,) =<< (<> "kuu"))
-                <$> [ "tammi"
-                    , "helmi"
-                    , "maalis"
-                    , "huhti"
-                    , "touko"
-                    , "kesä"
-                    , "heinä"
-                    , "elo"
-                    , "syys"
-                    , "loka"
-                    , "marras"
-                    , "joulu"
-                    ]
-        , amPm = ("ennen puoltapäivää", "puolen päivän jälkeen")
-        , dateTimeFmt = "%d.%m.%Y %H:%M:%S"
-        , dateFmt = "%d.%m.%Y"
-        , timeFmt = "%H:%M:%S"
-        , time12Fmt = "%H:%M:%S"
-        , knownTimeZones = []
+                <$> [ "tammi",
+                      "helmi",
+                      "maalis",
+                      "huhti",
+                      "touko",
+                      "kesä",
+                      "heinä",
+                      "elo",
+                      "syys",
+                      "loka",
+                      "marras",
+                      "joulu"
+                    ],
+          amPm = ("ennen puoltapäivää", "puolen päivän jälkeen"),
+          dateTimeFmt = "%d.%m.%Y %H:%M:%S",
+          dateFmt = "%d.%m.%Y",
+          timeFmt = "%H:%M:%S",
+          time12Fmt = "%H:%M:%S",
+          knownTimeZones = []
         }
 
 englishSite :: SiteConfig
 englishSite =
     defaultSiteConfig
-        { siteUrl = "https://loversguild.org/"
-        , pagesDir = [osp|pages/en|]
-        , outputDir = [osp|output/en|]
-        , dateFormat = "on %a, %d %b %Y"
+        { siteUrl = "https://loversguild.org/",
+          pagesDir = [osp|pages/en|],
+          outputDir = [osp|output/en|],
+          locales =
+            [   ( "en",
+                  LocaleConfig
+                    { dateFormat = "on %a, %d %b %Y",
+                      timeLocale = defaultTimeLocale
+                    }
+                )
+            ]
         }
 
 --------------------------
